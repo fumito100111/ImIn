84e28c018f34623b5fe4baba92c083df1e205fb0
fumito100111 <146402169+fumito100111@users.noreply.github.com>
2025-12-27 02:26:58 +0900

    feat: データベース設計 & ユーザー登録等に必要な関数の実装

---
+++ app/src/utils/db.py
@@ -0,0 +1,10 @@
+from ._db import (
+    create_users_table,
+    is_registered_user,
+    register_user,
+    update_user_state,
+    delete_user,
+    get_user_info,
+    get_users_by_state,
+    change_user_id
+)
