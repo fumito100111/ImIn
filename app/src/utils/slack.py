14d5ab7f1c7cb79a8dc9679b82a589abcb971d2e
fumito100111 <146402169+fumito100111@users.noreply.github.com>
2025-12-27 18:04:57 +0900

    feat: データベースの情報をもとにSlackのキャンパスを書き換えるように連携

--- app/src/utils/slack.py
+++ app/src/utils/slack.py
@@ -7,6 +7,8 @@ from urllib.parse import urlparse
 from slack_bolt import App as SlackApp
 from slack_sdk.web import SlackResponse
 from slack_sdk.errors import SlackApiError
+from ..utils import UserState, USER_STATE_LABELS
+from ..utils.db import get_users_by_state
 
 # SlackのセットアップについてのドキュメントURL
 SLACK_SETUP_DOCUMENT_URL: str = 'https://github.com/fumito100111/ImIn/blob/main/docs/SLACK_SETUP.md'
@@ -113,3 +115,26 @@ def replace_slack_canvas(service: str, content: str) -> bool:
         return False
 
     return True
+
+# データベースの情報をもとに, Slackのキャンパスの内容を置き換える
+def update_slack_canvas_from_db(root_dir: str, service: str) -> bool:
+    # ユーザー情報を取得する
+    in_users: list[dict[str, str]] = get_users_by_state(root_dir=root_dir, state=UserState.IN)
+    out_users: list[dict[str, str]] = get_users_by_state(root_dir=root_dir, state=UserState.OUT)
+
+    # Slackのキャンバスの内容を作成する
+    content: str = ''
+    # 在室ユーザー情報の追加
+    content += f'## {USER_STATE_LABELS[UserState.IN]}\n'
+    for user in in_users:
+        content += f'- {user['name']}\n'
+
+    content += '\n'
+
+    # 不在ユーザー情報の追加
+    content += f'\n## {USER_STATE_LABELS[UserState.OUT]}\n'
+    for user in out_users:
+        content += f'- {user['name']}\n'
+
+    # Slackのキャンバスを置き換える
+    return replace_slack_canvas(service=service, content=content)
