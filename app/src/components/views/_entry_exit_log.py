1452be77fa3a9f959ee885b308c165443f7d5f73
fumito100111 <146402169+fumito100111@users.noreply.github.com>
2025-12-29 02:01:46 +0900

    feat: NFCリーダーの接続状況の監視 & 読み取った情報のラベルへの反映を実装 (仮でUIDを表示している)

--- app/src/components/views/_entry_exit_log.py
+++ app/src/components/views/_entry_exit_log.py
@@ -6,6 +6,9 @@ from ...utils.nfc import NFC
 if TYPE_CHECKING:
     from ..windows import EntryExitLogWindow
 
+DEFAULT_MAIN_LABEL_TEXT: str = 'ICカードをかざしてください'
+DEFAULT_NOT_CONNECTED_TEXT: str = 'NFCリーダーが接続されていません'
+
 class ClockLabel(ctk.CTkLabel):
     def __init__(self, master: EntryExitLogView, width: int, height: int) -> None:
         super(ClockLabel, self).__init__(
@@ -29,6 +32,8 @@ class EntryExitLogView(ctk.CTkFrame):
     width: int
     height: int
     nfc: NFC
+    id_nfc_observer: str | None = None
+    main_label: ctk.CTkLabel
     clock_label: ClockLabel
     def __init__(self, master: ctk.CTkToplevel, width: int, height: int) -> None:
         super(EntryExitLogView, self).__init__(master=master, width=width, height=height)
@@ -36,10 +41,75 @@ class EntryExitLogView(ctk.CTkFrame):
         self.height = height
 
         # NFCの初期化
-        self.nfc = NFC()
+        self.nfc = NFC(command=self.callback_by_read_nfc_uid, only_once=False)
+
+        # メインラベルの作成
+        main_label_width = int(width * 0.8)
+        main_label_height = int(height * 0.2)
+        main_label_font_size = int(min(main_label_width, main_label_height) * 0.45)
+        self.main_label = ctk.CTkLabel(
+            master=self,
+            width=main_label_width,
+            height=main_label_height,
+            text='',
+            font=ctk.CTkFont(size=main_label_font_size),
+            anchor=ctk.CENTER,
+            fg_color='transparent',
+            bg_color='transparent'
+        )
+        self.main_label.place(relx=0.5, rely=0.5, anchor=ctk.CENTER)
 
         # 時計ラベルの作成
         clock_label_width = int(width * 0.8)
         clock_label_height = int(height * 0.2)
         self.clock_label = ClockLabel(self, width=clock_label_width, height=clock_label_height)
         self.clock_label.place(relx=0.95, rely=0.95, anchor=ctk.SE)
+
+        # NFCの接続状況を監視を開始
+        self._start_observe_nfc_connection()
+
+    # 入退室ログビューの終了
+    def destroy(self) -> None:
+        # NFCの接続状況の監視を停止
+        self._stop_observe_nfc_connection()
+
+        # NFCの読み取りセッションを停止
+        self.nfc.stop()
+
+        super(EntryExitLogView, self).destroy()
+
+    # NFCの接続状況を監視を開始
+    def _start_observe_nfc_connection(self) -> None:
+        # NFCの接続状況の監視を停止 (重複防止)
+        self._stop_observe_nfc_connection()
+
+        # NFCが接続されている場合, 監視を継続
+        if self.nfc.is_connected():
+            # NFCが接続されていなかった場合, NFCの読み取りセッションを開始
+            if self.main_label.cget('text') == '' or self.main_label.cget('text') == DEFAULT_NOT_CONNECTED_TEXT:
+                self.main_label.configure(text=DEFAULT_MAIN_LABEL_TEXT)
+                self.nfc.start()
+
+        # NFCが接続されていない場合, NFCの読み取りセッションを停止
+        else:
+            self.main_label.configure(text=DEFAULT_NOT_CONNECTED_TEXT)
+            self.nfc.stop()
+
+        # NFCの接続状況の監視を継続
+        self.id_nfc_observer = self.after(100, self._start_observe_nfc_connection)
+
+    # NFCの接続状況を監視を停止
+    def _stop_observe_nfc_connection(self) -> None:
+        if self.id_nfc_observer is not None:
+            self.after_cancel(self.id_nfc_observer)
+            self.id_nfc_observer = None
+
+    # NFCタグIDを読み取った時に呼ばれるコールバック関数
+    def callback_by_read_nfc_uid(self) -> None:
+        # メインラベルに読み取った情報を表示
+        self.main_label.configure(text=f'{self.nfc.response.uid}\n読み取りました')
+
+        # 1.0秒後にメインラベルをデフォルトの表示に戻す
+        def clear_main_label() -> None:
+            self.main_label.configure(text=DEFAULT_MAIN_LABEL_TEXT)
+        self.after(1000, clear_main_label)
